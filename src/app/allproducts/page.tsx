fa643af7e563174e12191b2d710ce77e8422ce92
rubii22 <m.yasirbro430@gmail.com>
2024-12-10 22:11:09 +0500

    git commit changes

--- src/app/allproducts/page.tsx
+++ src/app/allproducts/page.tsx
@@ -1,186 +1,54 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import p1 from "../../../public/p1.png"
-import p2 from "../../../public/p2.png"
-import p3 from "../../../public/p3.png"
-import p4 from "../../../public/p4.png"
-import p5 from "../../../public/p5.png"
-import p6 from "../../../public/p6.png"
-import p7 from "../../../public/p7.png"
-import p8 from "../../../public/p8.png"
-import p9 from "../../../public/p9.png"
-import p10 from "../../../public/p10.png"
-import p11 from "../../../public/p11.png"
-import p12 from "../../../public/p12.png"
-import p13 from "../../../public/p13.png"
-import p14 from "../../../public/p14.png"
-import p15 from "../../../public/p15.png"
-import p16 from "../../../public/p16.png"
-import p17 from "../../../public/p17.png"
-import p18 from "../../../public/p18.png"
-import p19 from "../../../public/p19.png"
-import p20 from "../../../public/p20.png"
-import p21 from "../../../public/p21.png"
-import p22 from "../../../public/p22.png"
-import p23 from "../../../public/p23.png"
-import p24 from "../../../public/p24.png"
-import p25 from "../../../public/p25.png"
-import p26 from "../../../public/p26.png"
-import p27 from "../../../public/p27.png"
-import p28 from "../../../public/p28.png"
-import p29 from "../../../public/p29.png"
-import p30 from "../../../public/p30.png"
-import p31 from "../../../public/p31.png"
-import p32 from "../../../public/p32.png"
-import Relatedimg from "../../../public/productrelated.png"
-import { ImImage } from 'react-icons/im'
-
+import React from "react";
+import Image from "next/image";
 
 const page = () => {
+  // Array of image imports for dynamic rendering
+  const images = Array.from({ length: 30 }, (_, i) => `/p${i + 1}.png`);
+
   return (
-    <div className=''>
-      <h1 className='text-xl font-bold ml-12'>New 500</h1>
-      <div className='flex justify-center gap-6'>
-        <div className=' w-64'>
-            <img src="/productsidebar.jpg" alt="" />
+    <div className="container mx-auto px-4 py-6">
+      {/* Heading */}
+      <h1 className="text-2xl font-bold text-center mb-6">New 500</h1>
+
+      {/* Main Content */}
+      <div className="flex flex-col lg:flex-row gap-6">
+        {/* Sidebar */}
+        <div className="lg:w-64 w-full">
+          <img
+            src="/productsidebar.jpg"
+            alt="Sidebar"
+            className="w-full h-auto rounded-lg"
+          />
         </div>
-        <div className=' w-[1100px] flex gap-5 flex-wrap'>
-            <div className='w-[348px] h-[535px]'>
-               
-                <Image src="/p1.png" alt='' width={348} height={535} />
+
+        {/* Product Grid */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
+          {images.map((src, index) => (
+            <div key={index} className="w-full">
+              <Image
+                src={src}
+                alt={`Product ${index + 1}`}
+                width={348}
+                height={535}
+                className="w-full h-auto rounded-lg shadow-md"
+              />
             </div>
-            <div className=' w-[348px] h-[535px]'>
-            <Image src="/p2.png" alt='' width={348} height={535} />
-            </div>
-            <div className=' w-[348px] h-[535px]'>
-
-            <Image src="/p3.png" alt='' width={348} height={535} />
-            </div>
-            <div className=' w-[348px] h-[535px]'>
-
-            <Image src="/p4.png" alt='' width={348} height={535} />
-            </div>
-            <div className=' w-[348px] h-[535px]'>
-
-            <Image src="/p5.png" alt='' width={348} height={535} />
-            </div>
-            <div className='w-[348px] h-[535px]'>
-
-            <Image src="/p6.png" alt='' width={348} height={535} />
-            </div>
-            <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p7.png" alt='' width={348} height={535} />
-           </div>
-
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p8.png" alt='' width={348} height={535} />
-           </div>
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p9.png" alt='' width={348} height={535} />
-           </div>
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p10.png" alt='' width={348} height={535} />
-           </div>
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p11.png" alt='' width={348} height={535} />
-           </div>
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p12.png" alt='' width={348} height={535} />
-           </div>
-           <div className='w-[348px] h-[535px]'>
-
-           <Image src="/p13.png" alt='' width={348} height={535} />
+          ))}
+        </div>
       </div>
 
-      <div className='w-[348px] h-[535px]'>
-
-<Image src="/p14.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p15.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p16.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p17.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p18.png" alt='' width={348} height={535} />
-</div>
-
-
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p19.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p20.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p21.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p22.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p23.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p24.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p25.png" alt='' width={348} height={535} />
-</div>
-
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p26.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p27.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p28.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p29.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-<Image src="/p30.png" alt='' width={348} height={535} />
-</div>
-<div className='w-[348px] h-[535px]'>
-
-</div>
- </div>
+      {/* Related Image */}
+      <div className="mt-8">
+        <Image
+          src="/productrelated.png"
+          alt="Related Products"
+          width={1029}
+          height={259}
+          className="w-full h-auto rounded-lg"
+        />
       </div>
-      <div className=''>
-            <Image src="/productrelated.png" alt='' width={1029} height={259} className='w-[1029px] h-[259px] ml-64 mt-2' />
-        </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
